9804930fe0e3d78a0007b35c5479df0537e0254c
jainath kumhar <jainathkumhar0@gmail.com>
2026-06-16 20:02:06 +0530

    feat(jwt): added jwt authentication

--- src/test/java/com/example/securityApp/securityApplication/SecurityApplicationTests.java
+++ src/test/java/com/example/securityApp/securityApplication/SecurityApplicationTests.java
@@ -1,13 +1,28 @@
 package com.example.securityApp.securityApplication;
 
+import com.example.securityApp.securityApplication.entity.User;
+import com.example.securityApp.securityApplication.services.JwtService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 
 @SpringBootTest
 class SecurityApplicationTests {
+	@Autowired
+	private JwtService jwtService;
 
 	@Test
 	void contextLoads() {
+		User user = new User(4L, "jainath@gmail.com", "12355");
+
+		String token = jwtService.generateToken(user);
+
+		System.out.println(token);
+
+		Long userId = jwtService.getUserIdFromToken(token);
+
+		System.out.println(userId);
 	}
 
 }
